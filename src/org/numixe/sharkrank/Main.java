05b86c0b87fd893231ea609255abc238f93de92b
Atlas97 <leleraffa97@hotmail.it>
2016-09-03 22:57:28 +0200

    aggiunta bukkit

--- src/org/numixe/sharkrank/Main.java
+++ src/org/numixe/sharkrank/Main.java
@@ -8,8 +8,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 	
-	public void onEnable() {}
-	public void onDisable() {}
+	public void onEnable() {
+		
+		
+	}
+	public void onDisable() {
+		
+		
+	}
 	
 	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args) {
 		
