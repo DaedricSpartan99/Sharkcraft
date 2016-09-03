4b4416d738c91ab266c8c82fee6375184f7c9eca
Numixe <numixe@gmail.com>
2016-09-03 22:36:14 +0200

    Progetto Matt

---
+++ src/org/numixe/sharkrank/Main.java
@@ -0,0 +1,25 @@
+package org.numixe.sharkrank;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	
+	public void onEnable() {}
+	public void onDisable() {}
+	
+	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args) {
+		
+		
+		return false;
+	}
+	
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		
+	}
+	
+
+}
