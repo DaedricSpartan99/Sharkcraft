ab16b83462197d47f9fb0e2e88456f680fd84fc8
Numixe <numixe@gmail.com>
2016-09-04 00:39:46 +0200

    Plugin Grezzo

--- src/org/numixe/sharkrank/Main.java
+++ src/org/numixe/sharkrank/Main.java
@@ -1,9 +1,10 @@
 package org.numixe.sharkrank;
 
-import java.io.File;
+import java.io.IOException;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -12,16 +13,16 @@ public class Main extends JavaPlugin {
 	public void onDisable() {}
 	
 	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args) {
-		
-		
 		Player p = (Player) sender;
-		if(p.getName().equalsIgnoreCase("lol")) {
-			File k = new File("kills.yml");
-			if (p.getKillCount() == 0) {
-				
-			}
-		}
-			
+		PlayerManager u = new PlayerManager(p.getName());
+		
+		if(p.getName().equalsIgnoreCase("shkill")) {
+			      try {
+					p.sendMessage("§c§lTEST >>§r" + "§a" + u + "ha " + u.loadKills());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			  }		
 		return false;
 	}
 	
---
+++ src/org/numixe/sharkrank/PlayerManager.java
@@ -0,0 +1,122 @@
+/**package org.numixe.sharkrank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerManager {
+
+	String name;
+	int kill_count;
+	
+	public PlayerManager(String name) {
+		
+		this.name = name;
+		kill_count = 0;
+	}
+
+	public void incKillCount() {
+		
+		kill_count++;
+	}
+	
+	public void setKillCount(int kill_count) {
+		
+		this.kill_count = kill_count;
+	}
+	
+	public int getKillCount() {
+		
+		return kill_count;
+	}
+	
+	public String getName() {
+		
+		return name;
+	}
+	
+	public void setName(String name) {
+		
+		this.name = name;
+	}
+	
+	public boolean loadKills() throws IOException {
+		
+		String line;
+		
+		InputStream fis = new FileInputStream("kills.yml");
+		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		BufferedReader br = new BufferedReader(isr);
+		
+		while ((line = br.readLine()) != null) {
+
+			if (line.startsWith(name)) {
+				
+				// name found
+				
+				kill_count = Integer.parseInt(line.split(":")[1]);
+				br.close();
+				return true;
+			}
+		}
+		
+		// no name found
+		
+		br.close();
+		kill_count = 0;
+		return false;
+	}
+	
+	
+	public void writeKill() throws IOException {
+		
+		String newline = name + ": " + String.valueOf(kill_count);
+		
+		// check for existing name
+		
+		List<String> lines = new ArrayList<String>();
+		
+		String line;
+		
+		InputStream fis = new FileInputStream("kills.yml");
+		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		BufferedReader br = new BufferedReader(isr);
+		
+		while ((line = br.readLine()) != null) {
+
+			if (!line.startsWith(name)) {
+				
+				lines.add(line);
+			}
+		}
+		
+		// no name found
+		
+		br.close();
+			
+		lines.add(newline);
+		
+		File kll = new File("kills.yml");	// delete old file
+		kll.delete();
+		kll.createNewFile();			// create new one
+		
+		FileWriter writer = new FileWriter("kills.yml", false);
+		BufferedWriter bw = new BufferedWriter(writer);
+        
+		for (String l : lines) {
+			
+			bw.write(l);
+			bw.newLine();
+		}
+		
+		bw.close();
+	}
+}**/
