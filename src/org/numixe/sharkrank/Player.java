e0c73371eaf617b72b6db71c34e0164ad2f6639c
Atlas97 <leleraffa97@hotmail.it>
2016-09-03 23:50:09 +0200

    loadKill() e writeKill()

--- src/org/numixe/sharkrank/Player.java
+++ src/org/numixe/sharkrank/Player.java
@@ -1,5 +1,13 @@
 package org.numixe.sharkrank;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 public class Player {
 
 	String name;
@@ -34,5 +42,74 @@ public class Player {
 	public void setName(String name) {
 		
 		this.name = name;
+	}
+	
+	public boolean loadKill() throws IOException {
+		
+		String line;
+		
+		InputStream fis = new FileInputStream("kills.yml");
+		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		BufferedReader br = new BufferedReader(isr);
+		
+		while ((line = br.readLine()) != null) {
+
+			if (line.startsWith(name)) {
+				
+				// name found
+				
+				kill_count = Integer.parseInt(line.split(":")[1]);
+				br.close();
+				return true;
+			}
+		}
+		
+		// no name found
+		
+		br.close();
+		kill_count = 0;
+		return false;
+	}
+	
+	public void writeKill() throws IOException {
+		
+		boolean name_found = false;
+		int nline = 0;
+		
+		String newline = name + ": " + String.valueOf(kill_count);
+		
+		// check for existing name
+		
+		String line;
+		
+		InputStream fis = new FileInputStream("kills.yml");
+		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		BufferedReader br = new BufferedReader(isr);
+		
+		while ((line = br.readLine()) != null) {
+
+			if (line.startsWith(name)) {
+				
+				// name found
+				
+				br.close();
+				name_found = true;
+				break;
+			}
+			
+			nline++;
+		}
+		
+		// no name found
+		
+		br.close();
+		
+		
+		
+		if (name_found) {
+			
+			FileWriter writer = new FileWriter("file.txt", false);
+	        writer.write("");
+		}
 	}
 }
