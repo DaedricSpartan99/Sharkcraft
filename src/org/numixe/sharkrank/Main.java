3db1c93831d40b0a7b3d4398c07ce64f0840ae45
Numixe <numixe@gmail.com>
2016-09-03 23:03:33 +0200

    Progetto Matt

--- src/org/numixe/sharkrank/Main.java
+++ src/org/numixe/sharkrank/Main.java
@@ -1,31 +1,27 @@
 package org.numixe.sharkrank;
 
+import java.io.File;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
 	
-	public void onEnable() {
-		
-		
-	}
-	public void onDisable() {
-		
-		
-	}
+	public void onEnable() {}
+	public void onDisable() {}
 	
 	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args) {
-		
-		
+		Player p = (Player) sender;
+		if(p.getName().equalsIgnoreCase("lol")) {
+			File k = new File("kills.yml");
+			if (p.getKillCount() = 0) {
+				
+			}
+		}
+			
 		return false;
 	}
 	
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		
-	}
 	
-
 }
