c1c747579c5cbd4f0dd9eda68dde6999fd56c4dd
Numixe <numixe@gmail.com>
2016-09-04 13:29:09 +0200

    Plugin Bukkit Funzionante

--- src/org/numixe/sharkrank/Main.java
+++ src/org/numixe/sharkrank/Main.java
@@ -1,34 +1,36 @@
 package org.numixe.sharkrank;
 
-import java.io.IOException;
-
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
-	
-	public void onEnable() {}
-	public void onDisable() {}
-	
-	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args) {
-		
-		Player p = (Player) sender;
-		PlayerManager u = new PlayerManager(p.getName());
-		
-		u.loadKills();		// carica dal file la kill_count relativa al giocatore
-		
-		if(p.getName().equalsIgnoreCase("shkill")) {
-			
-				p.sendMessage("�c�lTEST >>�r" + "�a" + u.getName() + "ha " + u.getKillCount());
-				
-				// stampa il nome del giocatore + la killcount
-				
-		}		
-		
-		return false;
-	}
-	
-	
+public class Main
+  extends JavaPlugin
+  implements Listener
+{
+  
+  public void onEnable()
+  {
+	    Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "SharkRank attivato");
+    getServer().getPluginManager().registerEvents(this, this);
+  }
+  
+  public void onDisable()
+  {
+    Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "SharkRank disattivato");
+  }
+  
+  public boolean onCommand(CommandSender sender, Command cmd, String CommandLabel, String[] args)
+  {
+    Player p = (Player) sender;
+    PlayerManager u = new PlayerManager(p.getName());
+    if (CommandLabel.equalsIgnoreCase("shkill")) {
+    	p.sendMessage(u.getName() + "Funziona: " + u.kill_count);
+      }
+    return false;
+  }
 }
